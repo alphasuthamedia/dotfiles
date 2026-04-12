724595a80138860a81051f0afbb8693bd52f21c3
alpha <alpha.sutha@ui.ac.id>
2026-04-13 05:50:09 +0700

    the true nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,15 @@
 vim.opt.number = true
-vim.opt.clipboard = "unnamedplus"
+
+-- " → default register
+-- "+ → clipboard OS
+-- "* → primary selection (Linux)
+-- "_ → black hole (buang)
+-- "0 → hasil yank terakhir
+-- vim.opt.clipboard = "unnamedplus"
+--- set -> mode / key / command
+-- vim.keymap.set("v", "p", '"_dP')
+
+-- biar langsung pindah sesuai yang pas + highligh possible
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
 require("config.lazy")
