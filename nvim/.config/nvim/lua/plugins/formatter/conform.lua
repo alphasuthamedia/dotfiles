639d197926ed53f129dd7fe60a5b53257f9a2961
Alpha SM <alpha.sutha@ui.ac.id>
2026-06-16 20:48:46 +0700

    + java formatter

--- nvim/.config/nvim/lua/plugins/formatter/conform.lua
+++ nvim/.config/nvim/lua/plugins/formatter/conform.lua
@@ -3,6 +3,10 @@ return {
 	event = { "BufWritePre" },
 	config = function()
 		require("conform").setup({
+			formatters_by_ft = {
+        			java = { "google-java-format" },
+			},
+
 			format_on_save = {
 				lsp_fallback = true,
 			},
